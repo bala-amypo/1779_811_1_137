954b3edb1aea15b891890322b5bdde36dde3c374
bala-amypo <bala@amypo.in>
2025-12-19 02:19:08 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/RoleEntity.java
@@ -0,0 +1,32 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "roleName")
+})
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String roleName;
+
+    private String description;
+
+    private boolean active = true;
+
+    // getters & setters
+    public Long getId() { return id; }
+
+    public String getRoleName() { return roleName; }
+    public void setRoleName(String roleName) { this.roleName = roleName; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public boolean isActive() { return active; }
+    public void setActive(boolean active) { this.active = active; }
+}
