d578713b96e7eca2246c99b8b88395f3889d2bce
bala-amypo <bala@amypo.in>
2025-12-22 05:41:32 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RoleServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.Role;
+import com.example.demo.exception.BadRequestException;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.RoleRepository;
 import com.example.demo.service.RoleService;
 import org.springframework.stereotype.Service;
@@ -15,24 +17,35 @@ public class RoleServiceImpl implements RoleService {
     public RoleServiceImpl(RoleRepository repo) {
         this.repo = repo;
     }
+
     @Override
-    public Role create(Role role) {
+    public Role create(Role role) 
+        if (repo.existsByRoleName(role.getRoleName())) {
+            throw new BadRequestException("Role name already exists");
+        }
         return repo.save(role);
     }
+
     @Override
     public Role get(Long id) {
-        return repo.findById(id).orElseThrow();
+        return repo.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Role not found with id " + id));
     }
+
     @Override
     public List<Role> all() {
         return repo.findAll();
     }
+
     @Override
     public Role update(Long id, Role role) {
         Role db = get(id);
+        db.setRoleName(role.getRoleName());
         db.setDescription(role.getDescription());
         return repo.save(db);
     }
+
     @Override
     public void deactivate(Long id) {
         Role db = get(id);
